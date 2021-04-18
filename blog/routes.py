760dedae47bed4b90601ec869f59cb793b3a0cb5
petec0x0 <udehonyedikachi01@gmail.com>
2021-04-18 20:00:06 +0200

    Authentication and Create Post Functionality

--- blog/routes.py
+++ blog/routes.py
@@ -1,34 +1,25 @@
 from blog import app, db
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from blog.forms import RegisterForm, LoginForm
 from blog.models import User, Post
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 
-posts = [
-        {
-            'author': 'Petec 0x0',
-            'title': 'First blog',
-            'content': 'This is the content of the first blog post on the flask blog',
-            'date_posted': 'March 29 2021'
-         },
-        {
-            'author': 'Onyedikachi',
-            'title': 'Techinical blog',
-            'content': 'THere you can get help of any object by pressing Ctrl+I in front of it, either on the Editor or the Console.',
-            'date_posted': 'March 30 2021'
-         }
-    ]
 
+# route to the home page
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
+
     return render_template('index.html', posts=posts)
 
+# about page route
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
+# register page route
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     register_form = RegisterForm()
@@ -57,6 +48,7 @@ def register():
 
     return render_template('register.html', title='Register', register_form=register_form)
 
+# login page route
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     # get the LoginForm class from form.py
@@ -83,9 +75,45 @@ def login():
             
     
     # check for error messages     
-    if form.errors:
+    if login_form.errors:
         # ilterate and flash error messages if it exist
-        for errors in form.errors.values():
+        for errors in login_form.errors.values():
             flash(f'Error : {errors[0]}', category='danger')
     
     return render_template('login.html', title='Login', login_form=login_form)
+
+# logout functionality route
+@app.route('/logout')
+@login_required
+def logout():
+    # render the market.html template
+    logout_user()
+    # flash info message about the logout
+    flash('Signed out successfully!', category='info')
+    # redirect to home page after logout
+    return redirect(url_for('home'))
+
+# create post page route
+@app.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    # check if a post have been created
+    if request.method == "POST":
+        # get the post request values
+        post_title = request.form.get('title')
+        post_slug = post_title.replace(' ', '-')
+        post_body = request.form.get('body')
+        post_author = current_user.username
+        # create an object of the Post class
+        post = Post(title=post_title, slug=post_slug, body=post_body, author=post_author)
+        
+        # commit post to the database
+        db.session.add(post)
+        db.session.commit()
+
+        # flash success message
+        flash('Post created successfully', category='success')
+        # redirect to the home page
+        return redirect(url_for('home'))
+
+    return render_template('create.html', title='Create Post')
